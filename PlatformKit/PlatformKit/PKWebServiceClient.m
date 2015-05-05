669cae17197976a1ffeaa20099657a7c19ea0eeb
Thong Nguyen <tumtumtum@gmail.com>
2015-05-05 01:29:04 +0100

    Removed deprecated stuff

--- PlatformKit/PlatformKit/PKWebServiceClient.m
+++ PlatformKit/PlatformKit/PKWebServiceClient.m
@@ -146,10 +146,7 @@ static NSOperationQueue* defaultOperationQueue;
     {
         NSDictionary* sslSettings = [NSDictionary dictionaryWithObjectsAndKeys:
                                      (NSString*)kCFStreamSocketSecurityLevelNegotiatedSSL, kCFStreamSSLLevel,
-                                     [NSNumber numberWithBool:YES], kCFStreamSSLAllowsExpiredCertificates,
-                                     [NSNumber numberWithBool:YES], kCFStreamSSLAllowsExpiredRoots,
-                                     [NSNumber numberWithBool:YES], kCFStreamSSLAllowsAnyRoot,
-                                     [NSNumber numberWithBool:NO], kCFStreamSSLValidatesCertificateChain,
+                                     [NSNumber numberWithBool:NO], kCFStreamSSLValidatesCertificateChain,                                    [NSNumber numberWithBool:NO], kCFStreamSSLValidatesCertificateChain,
                                      [NSNull null], kCFStreamSSLPeerName,
                                      nil];
 		
