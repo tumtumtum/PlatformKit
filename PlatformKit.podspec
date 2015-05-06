eaaa63b7960e78ccd7a2055d2f5dd06841882b37
Thong Nguyen <tumtumtum@gmail.com>
2015-05-06 03:32:07 +0100

    Added OSX build target

--- PlatformKit.podspec
+++ PlatformKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "PlatformKit"
-  s.version      = "0.1.3"
+  s.version      = "0.1.4"
   s.summary      = "A set of useful classes and extensions for modern Objective C"
   s.homepage     = "https://github.com/tumtumtum/PlatformKit/"
   s.license      = 'MIT'
---
+++ PlatformKit/PlatformKitMac/PlatformKitMac.h
@@ -0,0 +1,19 @@
+//
+//  PlatformKitMac.h
+//  PlatformKitMac
+//
+//  Created by Thong Nguyen on 06/05/2015.
+//  Copyright (c) 2015 Thong Nguyen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlatformKitMac.
+FOUNDATION_EXPORT double PlatformKitMacVersionNumber;
+
+//! Project version string for PlatformKitMac.
+FOUNDATION_EXPORT const unsigned char PlatformKitMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatformKitMac/PublicHeader.h>
+
+
