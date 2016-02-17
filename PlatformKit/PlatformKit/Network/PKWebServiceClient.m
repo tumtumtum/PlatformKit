11da08cf86a65037ba3e12f087457d40335dd2b7
Thong Nguyen <tum@Kastr-iMac-Tum.local>
2016-02-17 18:23:15 +0000

    Support adding any header in PKWebServiceClient

--- PlatformKit/PlatformKit/Network/PKWebServiceClient.m
+++ PlatformKit/PlatformKit/Network/PKWebServiceClient.m
@@ -105,24 +105,21 @@ static NSOperationQueue* defaultOperationQueue;
 		return 0;
 	}
 
-    NSString* value;
-    
-    value = [options objectForKey:@"Header/ContentType"] ?: @"application/json";
-    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Content-Type"), (__bridge CFStringRef)value);
-    
-    value = [options objectForKey:@"Header/Accept"] ?: @"application/json,*/*;";
-    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Accept"), (__bridge CFStringRef)value);
-    
-    value = [options objectForKey:@"Header/Accept-Encoding"] ?: @"gzip";
-    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Accept-Encoding"), (__bridge CFStringRef)value);
+    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Content-Type"), (__bridge CFStringRef)@"application/json");
+    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Accept"), (__bridge CFStringRef)@"application/json,*/*;");
+    CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Accept-Encoding"), (__bridge CFStringRef)@"gzip");
 
-	value = [options objectForKey:@"Header/Authorization"] ?: nil;
-
-    if (value)
-    {
-        CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Authorization"), (__bridge CFStringRef) value);
-    }
-
+	for (NSString* key in options.allKeys)
+	{
+		if ([key hasPrefix:@"Header/"])
+		{
+			NSString* value = [options objectForKey:key];
+			NSString* header = [key substringFromIndex:@"Header/".length];
+			
+			CFHTTPMessageSetHeaderFieldValue(message, (__bridge CFStringRef)header, (__bridge CFStringRef)value);
+		}
+	}
+	
     if (postData)
     {
         CFHTTPMessageSetHeaderFieldValue(message, CFSTR("Content-Length"), (__bridge CFStringRef)[NSString stringWithFormat:@"%d", (int)postData.length]);
